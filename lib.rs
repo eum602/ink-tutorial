05cbc7c1c3a9902fb7f2476d736970f7d6fd24aa
Erick <eum602@gmail.com>
2021-12-08 09:46:22 -0500

    1-variables-constructors: solved

--- lib.rs
+++ lib.rs
@@ -8,17 +8,27 @@ mod incrementer {
     #[ink(storage)]
     pub struct Incrementer {
         // ACTION: Create a storage value called `value` which holds a `i32`
+        value: i32,
     }
 
     impl Incrementer {
         #[ink(constructor)]
         pub fn new(init_value: i32) -> Self {
             // ACTION: Create a new `Incrementer` and set its `value` to `init_value`
+            Self {
+                value: init_value,
+            }
         }
 
         // ACTION: Create a second constructor function named `default`.
         //         It has no input, and creates a new `Incrementer` with its `value`
         //         set to `0`.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self {
+                value: 0,
+            }
+        }
 
         #[ink(message)]
         pub fn get(&self) {
