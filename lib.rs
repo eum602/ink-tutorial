0df33cb959c65ce8fe7b69d699801fde3a21dd50
Erick <eum602@gmail.com>
2021-12-09 21:44:50 -0500

    4-hashMap-caller(msg.sender): initial

--- lib.rs
+++ lib.rs
@@ -7,47 +7,60 @@ mod incrementer {
 
     #[ink(storage)]
     pub struct Incrementer {
-        // ACTION: Create a storage value called `value` which holds a `i32`
         value: i32,
+        // ACTION: Add a `HashMap` from `AccountId` to `i32` named `my_value`
     }
 
     impl Incrementer {
         #[ink(constructor)]
         pub fn new(init_value: i32) -> Self {
-            // ACTION: Create a new `Incrementer` and set its `value` to `init_value`
             Self {
-                value: init_value,
+				value: init_value,
+				// ACTION: Set initial `my_value`
             }
         }
 
-        // ACTION: Create a second constructor function named `default`.
-        //         It has no input, and creates a new `Incrementer` with its `value`
-        //         set to `0`.
         #[ink(constructor)]
         pub fn default() -> Self {
             Self {
                 value: 0,
+				// ACTION: Set initial `my_value`
             }
         }
 
         #[ink(message)]
-        // ACTION: Update this function to return an i32.
         pub fn get(&self) -> i32 {
-            // ACTION: Return the `value`.
             self.value
         }
 
         #[ink(message)]
         pub fn inc(&mut self, by: i32) {
-            // ACTION: Simply increment `value` by `by`
             self.value += by;
+        }
+
+        #[ink(message)]
+        pub fn get_mine(&self) -> i32 {
+            // ACTION: Get `my_value` using `my_value_or_zero` on `&self.env().caller()`
+            // ACTION: Return `my_value`
+        }
+
+        fn my_value_or_zero(&self, of: &AccountId) -> i32 {
+            // ACTION: `get` and return the value of `of` and `unwrap_or` return 0
         }
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
+
+        // Alias `ink_lang` so we can use `ink::test`.
         use ink_lang as ink;
+
+        #[ink::test]
+        fn default_works() {
+            let contract = Incrementer::default();
+            assert_eq!(contract.get(), 0);
+        }
 
         #[ink::test]
         fn it_works() {
@@ -57,6 +70,14 @@ mod incrementer {
             assert_eq!(contract.get(), 47);
             contract.inc(-50);
             assert_eq!(contract.get(), -3);
+        }
+
+        // Use `ink::test` to initialize accounts.
+        #[ink::test]
+        fn my_value_works() {
+            let contract = Incrementer::new(11);
+            assert_eq!(contract.get(), 11);
+            assert_eq!(contract.get_mine(), 0);
         }
     }
 }
